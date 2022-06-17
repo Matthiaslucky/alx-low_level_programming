d340d0053bcd816b5790d3e738c32a0dc5c7e2ec
Matthiaslucky <mtlucky32@gmail.com>
2022-06-17 11:11:34 -0700

    task 1

--- 1-strncat.c
+++ 1-strncat.c
@@ -1,4 +1,4 @@
-#include "main.h"
+nclude "main.h"
 
 /**
  * _strncat - Concatenates two strings using at most
@@ -17,5 +17,6 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
+
 	return (dest);
 }
